21e22bad93518baad1d7e14b21861f69c8b1aa8a
Kirill <kirk.terekhin@gmail.com>
2018-01-02 12:02:27 +0600

    Устанавливает панель Unity(Ubuntu).

--- devel/js/index.js
+++ devel/js/index.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+	var about = document.querySelector('.about');
 	var button = document.querySelector('.hamburger');
 	var nav = document.querySelector('.nav');
 	button.addEventListener('click', function() {
+		about.classList.toggle('active');
 		nav.classList.toggle('active');
 		button.classList.toggle('hamburger__active');
 	})
