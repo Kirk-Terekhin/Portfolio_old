376e7a87aae76fb2101c3722d4be2293332c7edb
Kirill <kirk.terekhin@gmail.com>
2018-01-04 12:05:09 +0600

    Заполняет портфолио работами.

--- devel/code/index.js
+++ devel/code/index.js
@@ -1,12 +1,88 @@
 document.addEventListener("DOMContentLoaded", function() {
+	var xs = 550,
+		sm = 768,
+		md = 960,
+		lg = 1150,
+		xlg = 1850;
+	var grid = 4;
 
-	var about = document.querySelector('.about');
-	var button = document.querySelector('.hamburger');
-	var nav = document.querySelector('.nav');
-	button.addEventListener('click', function() {
-		about.classList.toggle('active');
-		nav.classList.toggle('active');
-		button.classList.toggle('hamburger__active');
-	})
+	var portfolio__item = document.querySelectorAll('.portfolio__article');
+	var portfolio = document.querySelector('.portfolio');
+	var ifGrid = document.querySelectorAll('.grid__column');
+
+
+	navActive();
+	gridCreate();
+
+
+	function navActive() {
+		var about = document.querySelector('.about');
+		var nav = document.querySelector('.nav');
+		var button = document.querySelector('.hamburger');
+		button.addEventListener('click', function() {
+			about.classList.toggle('active');
+			nav.classList.toggle('active');
+			button.classList.toggle('hamburger__active');
+		})
+	}
+
+
+	function gridCreate() {
+		gridCount();
+		for (var i = 0; i < grid; i++) {
+			var itemGrid = document.createElement('div');
+			itemGrid.classList.add('grid__column');
+			portfolio.appendChild(itemGrid)
+		}
+		var gridColumn = document.querySelectorAll('.grid__column');
+		for (var i = 0; i < portfolio__item.length; i++) {
+			for (var j = 0; j < grid; j++) {
+				if (portfolio__item[grid * i + j]) {
+					gridColumn[j].appendChild(portfolio__item[grid * i + j]);
+				}
+			}
+		}
+	}
+
+	function gridCount() {
+		var windowWidth = window.innerWidth;
+		if (windowWidth < xs) {
+			grid = 1;
+		} else if (windowWidth < md) {
+			grid = 2;
+		} else if (windowWidth < lg) {
+			grid = 3;
+		} else if (windowWidth < xlg) {
+			grid = 4;
+		} else {
+			grid = 4;
+		}
+	}
+	window.addEventListener('resize', function(e) {
+		var ifGrid = document.querySelectorAll('.grid__column');
+		gridCount();
+		if (ifGrid.length !== grid) {
+			document.querySelector('.portfolio').innerHTML = '';
+			gridCreate();
+		}
+	});
+
+
+	function scrollWidth() {
+		var div = document.createElement('div');
+
+		div.style.overflowY = 'scroll';
+		div.style.width = '50px';
+		div.style.height = '50px';
+
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+		var scrollWidth = div.offsetWidth - div.clientWidth;
+		document.body.removeChild(div);
+
+		return scrollWidth;
+	}
+
 
 });
